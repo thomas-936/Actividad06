3178f131b09d35d8b28cc019514bbd591261573d
tomas <tomas27@gmail.com>
2025-07-15 06:24:32 -0600

    primer commit

--- Actividad_06.py
+++ Actividad_06.py
@@ -1,1 +1,19 @@
 print("Actidad 06")
+prductos = {}
+cantidad = int(input("Ingrese la cantidad de productos que desea ingresar: "))
+for i in range(cantidad):
+    print(f"\nPoducto #{i+1}: ")
+    codigo = input("Ingrese codigo: ")
+    nombre_producto = input("Ingrese el nomnbre de procducto: ")
+    categoria = input("Ingrese la categoria del producto: ")
+    talla = input("Ingrese la talla del producto: ")
+    precio_unitario = input("Ingrese el precio unitario del producto: ")
+    catidad_stock = input("Ingrese la cantidad en stock: ")
+
+    prductos[codigo]= {
+        "nombre del producto":nombre_producto,
+        "categoria":categoria,
+        "talla": talla,
+        "precio unitario": precio_unitario,
+        "cantidad en stock": catidad_stock,
+    }
